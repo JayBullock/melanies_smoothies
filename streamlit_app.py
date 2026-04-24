dec81f385f8cae7dbe752a7016ea0453e36ea0dc
JayBullock <jaybullock@kubrickgroup.com>
2026-04-24 14:19:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,14 +23,14 @@ st.write('The name on your Smoothie will be:', name_on_order)
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+pd_df = my_dataframe.to_pandas()
+# st.dataframe(pd_df)
+# st.stop()
 
 
 ingredients_list = st.multiselect('Chose up to 5 ingredients:', my_dataframe, max_selections=5)
 if ingredients_list:
-    my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-    pd_df = my_dataframe.to_pandas()
-    st.dataframe(pd_df)
-    st.stop()
     # st.write(ingredients_list)
     # st.text(ingredients_list)
 
